37aabafbac1487cacf63bdee8e5370c2f7d08da1
RIZKI DESU <s4y4pu1an6@gmail.com>
2024-03-14 07:31:30 +0700

    first commit

---
+++ pengenalan javascript/hitung.js
@@ -0,0 +1,6 @@
+
+let a = 1
+let b = 30
+let c = a+b
+
+console.log("hasil dari",a,"ditambah",b,"adalah:",c)
---
+++ pengenalan javascript/index.js
@@ -0,0 +1,2 @@
+let tanggal =Date()
+console.log("halloo siswa hari cerah pada :", tanggal)
